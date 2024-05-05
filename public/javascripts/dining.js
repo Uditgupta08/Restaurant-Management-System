c3eaf21667d65e9180a1336a644ec1cad53e1ede
Uditgupta08 <135364043+Uditgupta08@users.noreply.github.com>
2024-05-05 12:20:42 +0530

    Update dining.js

--- public/javascripts/dining.js
+++ public/javascripts/dining.js
@@ -6,30 +6,38 @@ fetch("/dining")
     data.tables.forEach((table) => {
       const li = document.createElement("li");
       li.innerHTML = `
-                        <strong>Table Name:</strong> ${table.table_name}<br />
-                        <strong>Seating Capacity:</strong> ${
-                          table.seating_capacity
-                        }<br />
-                        <strong>Status:</strong> ${
-                          table.occupied ? "Occupied" : "Empty"
-                        }<br />
-                    `;
+        <strong>Table Name:</strong> ${table.table_name}<br />
+        <strong>Seating Capacity:</strong> ${table.seating_capacity}<br />
+        <strong>Status:</strong> ${table.occupied ? "Occupied" : "Empty"}<br />
+      `;
       const updateStatusForm = document.createElement("form");
       updateStatusForm.action = `/dining/update-status/${table.table_id}`;
       updateStatusForm.method = "POST";
       updateStatusForm.innerHTML = `
-                        <button type="submit" name="occupied" value="${!table.occupied}">
-                            ${table.occupied ? "Vacate" : "Reserve"}
-                        </button>
-                    `;
+        <button type="submit" name="occupied" value="${!table.occupied}">
+          ${table.occupied ? "Vacate" : "Reserve"}
+        </button>
+      `;
       li.appendChild(updateStatusForm);
-      const deleteTableForm = document.createElement("form");
-      deleteTableForm.action = `/dining/delete/${table.table_id}`;
-      deleteTableForm.method = "POST";
-      deleteTableForm.innerHTML = `
-                        <button type="submit">Delete</button>
-                    `;
-      li.appendChild(deleteTableForm);
+      if (!table.occupied) {
+        const deleteButtonForm = document.createElement("form");
+        deleteButtonForm.id = `deleteForm${table.table_id}`;
+        deleteButtonForm.action = `/dining/delete/${table.table_id}`;
+        deleteButtonForm.method = "POST";
+        deleteButtonForm.innerHTML = `
+          <button type="button" class="deleteButton" data-tableid="${table.table_id}">Delete</button>
+        `;
+        li.appendChild(deleteButtonForm);
+      }
+
+      const vacateForm = document.createElement("form");
+      vacateForm.action = `/dining/vacate/${table.table_id}`;
+      vacateForm.method = "POST";
+      vacateForm.innerHTML = `
+        <button type="submit">Vacate</button>
+      `;
+      li.appendChild(vacateForm);
+
       tablesList.appendChild(li);
     });
   })
